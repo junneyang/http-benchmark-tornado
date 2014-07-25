a88c7a45b32ae488f8e593399f93a890765e7201
JunneJun <yangjun03@baidu.com>
2014-07-25 18:44:31 +0800

    vfdvdvdvd

--- easyhttpbenchmark/easystatclient.py
+++ easyhttpbenchmark/easystatclient.py
@@ -9,19 +9,19 @@ if __name__ == "__main__":
     print("""======================================================================================
 |                                 TEST REPORT                                 |
 ======================================================================================""")
-    print("""|  test_time               :"""+str(svr.get_test_time())+" min")
-    print("""|  total_req_cnt           :"""+str(svr.ret_total_req_cnt()))
-    print("""|  total_res_cnt           :"""+str(svr.ret_total_res_cnt()))
-    print("""|  total_err_cnt           :"""+str(svr.ret_total_err_cnt()))
-    print("""|  total_nul_cnt           :"""+str(svr.ret_total_nul_cnt()))
-    print("""|  QPS(query per second)   :"""+str(svr.ret_qps()))
+    print("""|  test_time               : """+str(svr.get_test_time())+" min")
+    print("""|  total_req_cnt           : """+str(svr.ret_total_req_cnt()))
+    print("""|  total_res_cnt           : """+str(svr.ret_total_res_cnt()))
+    print("""|  total_err_cnt           : """+str(svr.ret_total_err_cnt()))
+    print("""|  total_nul_cnt           : """+str(svr.ret_total_nul_cnt()))
+    print("""|  QPS(query per second)   : """+str("%0.2f" %svr.ret_qps()))
     print("""|-------------------------------------------------------------------------------------""")
 
-    print("""|  avg_res_time(ms)        :"""+str(svr.ret_avg_res_time()))
-    print("""|  below_10(ms)            :"""+str(svr.ret_total_below_10()))
-    print("""|  between_10_20(ms)       :"""+str(svr.ret_total_between_10_20()))
-    print("""|  between_20_30(ms)       :"""+str(svr.ret_total_between_20_30()))
-    print("""|  over_30(ms)             :"""+str(svr.ret_total_over_30()))
+    print("""|  avg_res_time(ms)        : """+str("%0.2f" %svr.ret_avg_res_time()))
+    print("""|  below_10(ms)            : """+str(svr.ret_total_below_10()))
+    print("""|  between_10_20(ms)       : """+str(svr.ret_total_between_10_20()))
+    print("""|  between_20_30(ms)       : """+str(svr.ret_total_between_20_30()))
+    print("""|  over_30(ms)             : """+str(svr.ret_total_over_30()))
     print("""======================================================================================""")
 
     try:
